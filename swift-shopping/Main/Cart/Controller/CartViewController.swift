987273f88f74ccdf9b959a927dece37f698eac27
13429892196 <463219366@qq.com>
2024-06-24 13:06:37 +0800

    视图控制器创建

---
+++ swift-shopping/Main/Cart/Controller/CartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CartViewController.swift
+//  swift-shopping
+//
+//  Created by cheng on 2024/6/24.
+//
+
+import UIKit
+
+class CartViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
