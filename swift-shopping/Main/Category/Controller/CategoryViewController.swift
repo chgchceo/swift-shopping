96d0f03889a0d29211b9efadbd1a66c596d74ae8
13429892196 <463219366@qq.com>
2024-06-26 10:32:55 +0800

    分类模块功能完成

--- swift-shopping/Main/Category/Controller/CategoryViewController.swift
+++ swift-shopping/Main/Category/Controller/CategoryViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class CategoryViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
+class CategoryViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDelegate,UICollectionViewDataSource {
     
     var cModel:CategoryModel?
 
     @IBOutlet weak var searchButt: UIButton!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var collectionView: UICollectionView!
+    var currentIndex:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,19 +32,26 @@ class CategoryViewController: BaseViewController, UITableViewDataSource, UITable
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        self.tableView.separatorStyle = .none
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.register(UINib.init(nibName: "CategoryCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CategoryCollectionViewCell")
         
     }
     
     func initData() -> Void {
         
+        self.showLoadingView()
         LNRequest.get(path: "https://smart-shop.itheima.net/index.php?s=/api/category/list", success: {request,res  in
             
             let model:CategoryModel = CategoryModel.deserialize(from: res as? [String:Any])!
             
+            self.hiddenLoadingView()
             if(model.status == 200){
                 
                 self.cModel = model
                 self.tableView.reloadData()
+                self.collectionView.reloadData()
             }
         })
     }
@@ -64,13 +72,48 @@ class CategoryViewController: BaseViewController, UITableViewDataSource, UITable
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        var cell:UITableViewCell = UITableViewCell()
+        let cell:UITableViewCell = UITableViewCell()
         let model:CategoryListModel = (self.cModel?.data?.list?[indexPath.row])!
         cell.textLabel?.text = model.name
         cell.textLabel?.font = UIFont.systemFont(ofSize: 13)
         cell.textLabel?.textColor = UIColor.gray
         cell.backgroundColor = UIColor.groupTableViewBackground
-        
+        if(indexPath.row == self.currentIndex){
+            cell.backgroundColor = UIColor.white
+            cell.textLabel?.textColor = UIColor.red
+        }
+        cell.textLabel?.textAlignment = NSTextAlignment.center
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.currentIndex = indexPath.row
+        self.tableView.reloadData()
+        self.collectionView.reloadData()
+    }
+    
+    
+    @available(iOS 6.0, *)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        
+        return self.cModel?.data?.list?[self.currentIndex].children?.count ?? 0
+    }
+    
+    @available(iOS 8.0, *)
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        let cell:CategoryCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryCollectionViewCell", for: indexPath) as! CategoryCollectionViewCell
+        
+        
+        let model:CategoryChildrenModel = self.cModel?.data?.list?[self.currentIndex].children?[indexPath.item] ?? CategoryChildrenModel()
+        
+        cell.megLab.text = model.name
+        let imgUrl = model.image?.preview_url
+        cell.imgView.sd_setImage(with: URL(string: imgUrl ?? ""))
+        return cell
+        
+    }
+
+    
 }
---
+++ swift-shopping/Main/Category/View/CategoryCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryCollectionViewCell.swift
+//  swift-shopping
+//
+//  Created by cheng on 2024/6/26.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    
+    @IBOutlet weak var megLab: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
